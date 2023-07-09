2a48328dd79ee5d897393225ff9725e823e630f7
masoud <masoudrng2@gmail.com>
2023-07-09 11:56:13 +0330

    add user

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
-var animal=[
-{id:1,namee:"gav",age:12},
-{id:2,namee:"mamd",age:122},
-
+var users=[
+{id:1,namee:"masouf",family:"ranj"},
+{id:2,namee:"mamad",family:"mokhtari"},
 ];
+console.log(users)
+
+var newid=prompt("id: ")
+var newnamee=prompt("name:")
+var newfamily=prompt("family")
+
+var addUser={
+id: newid,
+namee: newnamee,
+family: newfamily,
+}
+
+users.push(addUser)
+console.log(users)
