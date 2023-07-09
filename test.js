c877dd5633f8e0292a9ba2fb50aa21a08eab0e84
masoud <masoudrng2@gmail.com>
2023-07-09 12:39:51 +0330

    add use

--- test.js
+++ test.js
@@ -2,17 +2,7 @@ var users=[
 {id:1,namee:"masouf",family:"ranj"},
 {id:2,namee:"mamad",family:"mokhtari"},
 ];
-console.log(users)
+users.forEach(function(user){
+ console.log("id : " + user.id)
 
-var newid=prompt("id: ")
-var newnamee=prompt("name:")
-var newfamily=prompt("family")
-
-var addUser={
-id: newid,
-namee: newnamee,
-family: newfamily,
-}
-
-users.push(addUser)
-console.log(users)
+})
