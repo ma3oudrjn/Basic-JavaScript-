16c73477d91220e0a7970b402e21eeb985850cfd
masoud <masoudrng2@gmail.com>
2023-07-09 15:32:43 +0330

    mojang

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
-
-var database=[
-{id:12,name:"masoud",family:"ranjbaran"},
-{id:135,name:"ali",family:"havig abadi"},
-{id:94,name:"hossein",family:"kosid abadi"}
-]
-
-console.table([1,2,3,4,6])
+function inw(x,y){
+    var q=x+y
+return q
+}
+alert(inw(9,1))
