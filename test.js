7c3bfd9d0819a7f2e03912b9aeb78f4496a76a76
masoud <masoudrng2@gmail.com>
2023-07-09 16:32:53 +0330

    some

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-var araye=[
-{id:1,namee:'apple',price:1.2},
-{id:2,namee:'coconut',price:5}
-];
-var find=araye.some(function(a){
-return a.namee =='coconut'
-})
-console.log(find)
+//var frute=[
+//{id:1,namee:'coconut',price:5},
+//{id:2,namee:'apple',price:1},
+//{id:3,namee:'onion',price:0.4},
+//{id:4,namee:'orange',price:3},
+//{id:5,namee:'bannana',price:10}
+//];
+//var caculate=frute.some(function(x){
+
+
+
+
+
+var price=0
+//})
+for(var i=0;i<5;i++){
+
+    var price = price + Number(prompt('please set price'))
+}
+alert(price)
