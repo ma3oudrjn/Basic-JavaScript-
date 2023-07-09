ec724100d7fa042ac45386cbc39f6e66c8703b27
masoud <masoudrng2@gmail.com>
2023-07-09 11:00:27 +0330

    all

---
+++ array.js
@@ -0,0 +1,8 @@
+//array
+
+var x=[12,14,53,67,866,43]
+console.log(x)
+console.log(x.pop())
+console.log(x.push(12,13,14,15))
+
+
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var x=['masoud',3861334771,'ranjbaran',true]
 console.log(x)
-    
+console.log(x.length)
+
 //end
