278dd66d9237ea772f7573105e0fbdba54617346
masoud <masoudrng2@gmail.com>
2023-07-10 10:43:14 +0330

    first part

--- toDoList.js
+++ toDoList.js
@@ -4,4 +4,33 @@ var list=[
 {id:2,name:"study",status:true},
 {id:3,name:"make cake",status:true},
 ];
-var doin=prompt("1= add to do \n 2= remove \n 3= change status")
+
+
+
+var doing=prompt("1= add to do \n 2= remove \n 3= change status")
+
+// var add=[
+//     {id:Math.random()*100,}
+// ]
+// add=Math.trunc(add)
+// console.log(add);
+
+if(doing==='1'){
+    var addNewtext = prompt('plese add to do')
+
+    var newToDo={
+        //var addNewtext = prompt('plese add'),
+    id: list.length+1,
+    name: addNewtext,
+    status: false
+    }
+list.push(newToDo)
+console.log(list)
+}else if(doing==='2'){
+
+
+}else if(doing==='3'){
+
+
+}else
+console.log("pelese insert correct parameter!!!!!!!!");
