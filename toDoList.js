fb038c98d62508e304129b25ad1606dad7635e8c
masoud <masoudrng2@gmail.com>
2023-07-10 10:09:50 +0330

    add list

--- toDoList.js
+++ toDoList.js
@@ -1,1 +1,7 @@
-console.log("hi");
+//No.130
+var list=[
+{id:1,name:"coding",status:true},
+{id:2,name:"study",status:true},
+{id:3,name:"make cake",status:true},
+];
+var doin=prompt("1= add to do \n 2= remove \n 3= change status")
