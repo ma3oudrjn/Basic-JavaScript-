ec1249e1dcf46c53fb4472e994f34cf4bf85b955
masoud <masoudrng2@gmail.com>
2023-07-10 09:36:39 +0330

    same character

--- test.js
+++ test.js
@@ -1,23 +1,11 @@
-var product=[
-{id:1,Name:'laptop',Price:100},
-{id:2,Name:'phone',Price:120},
-{id:3,Name:'mouse',Price:12},
-{id:4,Name:'kyboard',Price:14},
-{id:5,Name:'monitor',Price:110},
-{id:6,Name:'coolpad',Price:28}
+var input=prompt("insert name")
+var self=input.split('')
+var w=self.reverse();
+var reversstring=self.join('')
+console.log(reversstring);
+if(reversstring==input){
 
-];
-var off =product.filter(function(x){
-
-return x.Price>99
-})
-
-var sell=off.length*5
-
-var some =0
-
-var final=product.forEach(function(y){
-some = some + y.Price
-})
-var total = some+sell
-console.log(total);
+    console.log("same");
+}else{
+    console.log("not same");
+}
