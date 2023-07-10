2d0c2ef6c0a65d8c87bedb2481dbfc6c37a7f840
masoud <masoudrng2@gmail.com>
2023-07-10 09:14:56 +0330

    totalsell

--- test.js
+++ test.js
@@ -1,20 +1,23 @@
-//var frute=[
-//{id:1,namee:'coconut',price:5},
-//{id:2,namee:'apple',price:1},
-//{id:3,namee:'onion',price:0.4},
-//{id:4,namee:'orange',price:3},
-//{id:5,namee:'bannana',price:10}
-//];
-//var caculate=frute.some(function(x){
+var product=[
+{id:1,Name:'laptop',Price:100},
+{id:2,Name:'phone',Price:120},
+{id:3,Name:'mouse',Price:12},
+{id:4,Name:'kyboard',Price:14},
+{id:5,Name:'monitor',Price:110},
+{id:6,Name:'coolpad',Price:28}
 
+];
+var off =product.filter(function(x){
 
+return x.Price>99
+})
 
+var sell=off.length*5
 
+var some =0
 
-var price=0
-//})
-for(var i=0;i<5;i++){
-
-    var price = price + Number(prompt('please set price'))
-}
-alert(price)
+var final=product.forEach(function(y){
+some = some + y.Price
+})
+var total = some+sell
+console.log(total);
